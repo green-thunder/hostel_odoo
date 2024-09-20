f1abe17723dad650ce228f9d79b11cdd7a5d8555
n.kamalov <n.kamalov@uic.group>
2024-09-20 10:32:45 +0500

    used delegation inheritance

--- models/hostel_student.py
+++ models/hostel_student.py
@@ -2,6 +2,8 @@ from odoo import models, fields
 
 class HostelStudent(models.Model):
     _name = 'hostel.student'
+    _inherits = {'res.partner': 'partner_id'}
+    _description = 'Hostel student information'
 
     name = fields.Char('Student name')
     gender = fields.Selection([
@@ -14,6 +16,6 @@ class HostelStudent(models.Model):
     room_id = fields.Many2one('hostel.room', 'Room', help='Select hostel room')
 
     hostel_id = fields.Many2one('hostel.hostel', related='room_id.hostel_id')
-
+    partner_id = fields.Many2one('res.partner', ondelete='cascade')
     
 
