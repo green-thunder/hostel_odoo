325479c20b63f8bd765d5d65406ad756eaec9bc8
n.kamalov <n.kamalov@uic.group>
2024-09-20 15:13:40 +0500

    fix: abstract model

--- models/hostel_rooms.py
+++ models/hostel_rooms.py
@@ -1,6 +1,14 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 from datetime import timedelta
+class BaseArchive(models.AbstractModel):
+    _name = 'base.archive'
+    active = fields.Boolean(default=True)
+
+    def do_archive(self):
+        for record in self:
+            record.active = not record.active
+
 
 class HostelRoom(models.Model):
     _name = 'hostel.room'
