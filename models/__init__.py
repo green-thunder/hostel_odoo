760518c2a9264432f97d7190bf92c90787c9b189
n.kamalov <n.kamalov@uic.group>
2024-09-20 10:09:30 +0500

    copied the hostel_room model

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from . import hostel_amenities
 from . import hostel_student
 from . import hostel_categ
 from . import hostel_book
+from . import hostel_room_copy
---
+++ models/hostel_room_copy.py
@@ -0,0 +1,9 @@
+from odoo import fields, models, api, _
+
+class HostelRoomCopy(models.Model):
+    _name = 'hostel.room.copy'
+    _inherit = 'hostel.room'
+    _description = 'Hostel room information copy'
+
+
+    
